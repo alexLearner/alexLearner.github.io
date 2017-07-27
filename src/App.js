da38ae5672bde28c05880a80683fc805f11fae01
alexLearner <dikayasobaka1>
2017-07-27 19:45:08 +0300

    + sass/scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import s from './app.sass';
+
+console.log(s)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={s.App}>
+        <div className={s["App-header"]}>
+          <img src={logo} className={s["App-logo"]} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
